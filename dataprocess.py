ab2fdf95d4e21876514a6523b0620ec90613fc85
David L. King <king.2138@osu.edu>
2018-03-29 15:38:52 -0400

    pretty printing for trees and tree building prototyped

--- dataprocess.py
+++ dataprocess.py
@@ -4,6 +4,9 @@
 import sys
 import argparse
 import pdb
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
 # import cPickle as pkl
 
 print("Sample usage: python3 datapreprocess.py -i file.conll")
@@ -20,6 +23,7 @@ class template:
         self.get_rel = lambda x: x.split('\t')[7]
         self.get_head = lambda x: x.split('\t')[6]
         self.get_dep = lambda x: x.split('\t')[0]
+        self.get_deps = lambda x, y: [z for z in y if self.get_head(z) == self.get_dep(x)]
 
     def sep(self, lines):
         sents = []
@@ -34,10 +38,35 @@ class template:
             sents.append(sent)
         return sents
 
+    def ptree(self, tree):
+        start = ''
+        for dep in tree:
+            if type(dep[0]) == str:
+                print(dep[0])
+
+    def tree_it(self, root, sent):
+        head = root[0]
+        if self.get_deps(head, sent) == []:
+            return head
+        else:
+            deps = self.get_deps(head, sent)
+            for d in deps:
+                sent.pop(sent.index(d))
+            for d in deps:
+                d = [d]
+                d = self.tree_it(d, sent)
+                root.append(d)
+            return root
+
     def find_root(self, sent):
-        pass
+        root = [x for x in sent if self.get_rel(x) == 'root']
+        assert(len(root) == 1)
+        sent.pop(sent.index(root[0]))
+        return root, sent
 
     def build_hier(self, sent):
+        root, sent = self.find_root(sent)
+        tree = self.tree_it(root, sent)
         pdb.set_trace()
 
     def load_file(self, _file):
